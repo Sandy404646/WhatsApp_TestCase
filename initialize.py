beef01ac9cf45683969df047b9559b3ab854aa30
sskhy <sandyhyper46@gmail.com>
2025-03-11 22:26:59 +0530

    demo commit

--- initialize.py
+++ initialize.py
@@ -7,6 +7,7 @@ spark = SparkSession.builder.appName("SimplePySparkJob").getOrCreate()
 input_file = "E:/Data Engineering/Python Samples/Employee Sample Data.xlsx"
 df = spark.read.csv(input_file, header=True, inferSchema=True)
 
+##filtering first name
 df.filter(col("firstname")=="John")
 
 ## To display

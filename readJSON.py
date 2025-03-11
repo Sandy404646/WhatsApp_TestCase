4627cd2fea476a753b91289f74fff358a3c0a4d5
sskhy <sandyhyper46@gmail.com>
2025-03-11 22:38:00 +0530

    json added

--- readJSON.py
+++ readJSON.py
@@ -3,9 +3,9 @@ from pyspark.sql.functions import *
 # Create a Spark session
 spark = SparkSession.builder.appName("SimplePySparkJob").getOrCreate()
 
-# Read a CSV file into a DataFrame
+# Read a JSON file into a DataFrame
 input_file = "E:/Data Engineering/Python Samples/Employee Sample Data.xlsx"
-df = spark.read.csv(input_file, header=True, inferSchema=True)
+df = spark.read.json(input_file)
 
 ##filtering first name
 df.filter(col("firstname")=="John")
